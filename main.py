a672fc9563ed3a1d22ab65c4532c233c87acdeef
NegarinJavadi <negarinjavadi@gmail.com>
2023-12-25 23:51:21 +0330

    por nakardane data

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from api.oanda_api import OandaApi
 from infrastructure.instrument_collection import instrumentCollection
 from simulation.ma_cross import run_ma_sim
+from simulation.ma_cross import run_ema_macd
 from dateutil import parser
 from infrastructure.collect_data import run_collection
 
 
 if __name__ == '__main__':
-    run_ma_sim()
+#    run_ma_sim()
 #    api = OandaApi()
-#    instrumentCollection.LoadInstruments("./data")
+    instrumentCollection.LoadInstruments("./data")
+    run_ema_macd(instrumentCollection)
 #    run_collection(instrumentCollection, api)
