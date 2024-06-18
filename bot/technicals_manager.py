004e34ecf216a1a196ee4576c93698c300830a70
NegarinJavadi <negarinjavadi@gmail.com>
2024-06-18 13:00:44 +0330

    final-v6

--- bot/technicals_manager.py
+++ bot/technicals_manager.py
@@ -19,9 +19,9 @@ ADDROWS = 20
 def apply_signal(row, trade_settings: TradeSettings):
 
     if row.SPREAD <= trade_settings.maxspread and row.GAIN >= trade_settings.mingain:
-        if row.mid_c > row['BB_UP'] and row.mid_o < row['BB_UP']:
+        if row.mid_c > row.BB_UP and row.mid_o < row.BB_UP:
             return defs.SELL
-        elif row.mid_c < row['BB_LW'] and row.mid_o > row['BB_LW']:
+        elif row.mid_c < row.BB_LW and row.mid_o > row.BB_LW:
             return defs.BUY
     return defs.NONE
 
@@ -46,7 +46,7 @@ def process_candles(df: pd.DataFrame, pair, trade_settings:TradeSettings, log_me
     df['SPREAD'] = df.ask_c- df.bid_c
 
     df = BollingerBands(df, trade_settings.n_ma, trade_settings.n_std)
-    df ['GAIN'] = abs(df.mid_c - df['BB_MA'])
+    df ['GAIN'] = abs(df.mid_c - df.BB_MA)
     df['SIGNAL'] = df.apply(apply_signal, axis=1, trade_settings=trade_settings)
     df['TP'] = df.apply(apply_TP, axis=1)
     df['SL'] = df.apply(apply_SL, axis=1, trade_settings=trade_settings)
